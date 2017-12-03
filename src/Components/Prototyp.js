48810161ee68c04747e9250cf449f93e8c2716ca
Leonard Follner <leonard.follner@gmail.com>
2017-12-03 16:53:10 +0100

    UPDATE: basic project structure

---
+++ src/Components/Prototyp.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import '../sass/Organisms/_prototyp.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default App;
