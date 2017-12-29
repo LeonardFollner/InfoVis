648ccfe8f9bc56bdc1c136be67dd7cdfd53ce0ee
Leonard Follner <leonard.follner@gmail.com>
2017-12-29 18:16:34 +0100

    TASK: remove oldest card on map if there are more than n cards removed separation of regions in storage

--- src/Redux/Data/terms.js
+++ src/Redux/Data/terms.js
@@ -4,7 +4,7 @@ import {$add, $drop, $get, $pop, $set, $shift} from 'plow-js';
 import {handleActions} from '../../Utility/HandleActions';
 import {actionTypes as system} from '../System';
 import data from '../../data.js';
-import {TargetRegions} from "../../constants";
+import {maxNumberOfCardsOnMap} from "../../settings";
 
 // action types
 
@@ -18,8 +18,7 @@ export const reducer = handleActions({
   [system.INIT]: () => state => {
     state = $set(['data', 'terms'], data, state);
     state = $set(['data', 'cardsInSideBar'], [], state);
-    state = $set(['data', 'cardsOnMap', TargetRegions.EUROPE], [], state);
-    state = $set(['data', 'cardsOnMap', TargetRegions.ARAB], [], state);
+    state = $set(['data', 'cardsOnMap'], [], state);
     for (let i = 0; i < 5; i++) {
       const term = $get(0, terms(state));
       state = $add(['data', 'cardsInSideBar'], term, state);
@@ -29,12 +28,22 @@ export const reducer = handleActions({
   },
   [CARD_DROPPED]: id => state => {
     const cardsInSideBar = cardsInSidebar(state);
+
+    // remove term from SideBar and move to mapRegion
     cardsInSideBar.forEach((card, index) => {
       if (card.id === id) {
         state = $drop(['data', 'cardsInSideBar', index], state);
-        state = $add(['data', 'cardsOnMap', card.targetRegion], card, state);
+        state = $add(['data', 'cardsOnMap'], card, state);
       }
     });
+
+    // if more than n items on map remove first added element and add back to pool
+    if (cardsOnMap(state).length > maxNumberOfCardsOnMap) {
+      const oldestTerm = $get(0, cardsOnMap(state));
+      state = $shift(['data', 'cardsOnMap'], state);
+      state = $add(['data', 'terms'], oldestTerm, state);
+    }
+
     // add a new term from the pool to the sidebar
     if (terms(state).length > 0) {
       const newTermForSideBar = $get(terms(state).length - 1, terms(state));
@@ -54,7 +63,7 @@ const cardsOnMapPerRegion = () => {
     cardsOnMap,
     (_, region) => region,
     (cardsOnMap, region) => {
-      return $get([region], cardsOnMap);
+      return cardsOnMap.filter(elem => elem.targetRegion === region);
     }
   );
 };
