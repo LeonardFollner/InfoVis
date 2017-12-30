2b40487b6b8e178ccbd93443e2404db24848dd70
Leonard Follner <leonard.follner@gmail.com>
2017-12-30 13:08:09 +0100

    TASK: added simple map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,9 +1,35 @@
 import React, {Component} from 'react';
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibGVvbmFyZC1mb2xsbmVyIiwiYSI6ImNqOXp5cnNwODh1MTkycWxnZHJnbnk2Z2IifQ.qFUBQPX9proV_Bj0mvdk2A';
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: 27.8770,
+      lat: 41.7667,
+      zoom: 3.25
+    };
+  }
+
+  componentDidMount() {
+    const {lng, lat, zoom} = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+    map.scrollZoom.disable();
+    map.dragPan.disable();
+  }
+
   render() {
     return (
-      <div className="map"/>
+      <div ref={el => this.mapContainer = el} className="map absolute top right left bottom"/>
     );
   }
 }
--- src/Components/MapContainer.js
+++ src/Components/MapContainer.js
@@ -1,16 +1,12 @@
 import React, {Component} from 'react';
 
-import Region from "./Region";
 import Map from "./Map";
-import {TargetRegions} from "../constants";
 
 class MapContainer extends Component {
   render() {
     return (
       <div className="map-container">
         <Map/>
-        <Region label={TargetRegions.EUROPE}/>
-        <Region label={TargetRegions.ARAB}/>
       </div>
     );
   }
