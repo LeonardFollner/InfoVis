12639282c40d1ec03b712db340db2d5d3697f758
anselmpaul <anselm.bunsen@tu-dresden.de>
2018-01-17 14:15:41 +0100

    added 3 arab words

--- src/data.js
+++ src/data.js
@@ -3,10 +3,25 @@ import {TargetRegions} from "./constants";
 const data = [
     {
         "id": 0,
-        "targetRegion": TargetRegions.EUROPE
+        "name": "Alkohol",
+        "arab": "",
+        "description": "Alkohol meist Getränke die betrunken machen",
+        "changes": 4,
+        "firstChange": "Die erste Gewinnung von konzentriertem Ethanol gelang dem persischen Arzt, Naturwissenschaftler, Philosophen  und Schriftsteller Abu Bakr Mohammad Ibn Zakariya al-Razi durch die Destillation von Wein.",
+        "secondChange": "Ursprünglich Pulver, sogeannt Spießglanzpulver, das aus Antimon hergestellt wurde und zum Einfärben der Wimpern, Augenbrauen und Lider benutzt wurde.",
+        "thirdChange": "Im 16. Jahrhundert existierte in Alchemistenkreisen bereits das Wort Alcohol zur Bezeichnung von feinem Pulver.",
+        "forthChange": "Auf den Branntwein wurde es dadurch übertragen, dass man von der Wortverbindung alcool vini (Destillat des Weines) den Bestandteil vini wegließ.",
+        "targetRegion": TargetRegions.ARAB
     },
     {
         "id": 1,
+        "name": "Giraffe",
+        "arab": "",
+        "changes": 4,
+        "firstChange": "Giraffe geht übers Italienische auf zarafa zurück. Das Wort ist im Arabischen aber ein Fremdwort.",
+        "secondChange": "Es könnte, der Herkunft des Tiers entsprechend, aus einer äthiopischen Sprache stammen, im alt-äthiopischen Ge'ez wird die Giraffe zarat genannt. Nun waren aber Giraffenarten vor einigen Jahrtausenden noch weiter nördlich als heute verbreitet und im alten Ägypten durchaus bekannt, wie Abbildungen und sogar eine Hieroglyphe zeigen. Zafara könnte deshalt auch auf ein unbekanntes ägyptisches Wort zurückgehen.",
+        "thirdChange": "In Europa kennt man die Giraffe nur aus spärlichen Textstellen der Antike und im Lauf der Zeit dann auch aus Berichten anlässlich von Kreuzzügen, Kauffahrts- und Pilgerreisen. Es ist daher nicht verwunderlich, dass zafara ab Mitte des 13. Jh. in unterschiedliche Formen in europäische Sprachen übernommen wurden.",
+        "forthChange": "In Marco Polos Reisebuch ist 1298 zum ersten Mal die Giraffe beschrieben. Durch Übersetzungen selbiger ins deutsche kommt der Begriff ins Deutsche."
         "targetRegion": TargetRegions.ARAB
     },
     {
@@ -15,6 +30,13 @@ const data = [
     },
     {
         "id": 3,
+        "name": "Elixir",
+        "arab": "",
+        "changes": 4,
+        "firstChange": "Während man heute unter Elixier meistens nicht einen Heiltrank sondern eher ein 'Heiltrank', versteht, geht das Wort urspürnglich auf das arabische al-iksir zurück. Mit diesem Begriff wurde Pulver zur Behandlung von Augenkrankheiten oder auch allgemein Wundbehandlung bezeichnet.",
+        "secondChange": "iksir wurde mitte des 9. Jh zu einem Schlüsselbegriff der arabischen Alchimie, wo es statt eines medizinischen Pulvers die Substanz bezeichnet, die unedle Metalle in Silber und Gold verwandeln helfe.",
+        "thirdChange": "Durch die große Geheimnistuerei um solche Mittel entstand auch der Mythos um den 'Stein der Weisen'. Im 12. und 13. Jahrhundert gelang der Begriff durch Übersetzungen arabischer Schriften in die europäischen Alchemie, wo man auch schon von Elixier spricht.",
+        "forthChange": "In Deutschland erscheint es zuerst in einem Volkslied im 15. Jh, und in einem Buch über die Kunst des Destillierens. In der folgenden Zeit setzt sich der Begriff Elixier in der Medizin als Begriff für Tinktur und Trank durch, 'das Elixier, eine auserlesene Arznei'."
         "targetRegion": TargetRegions.ARAB
     },
     {
