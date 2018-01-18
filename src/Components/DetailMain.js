0e6f81306568e961e7433152990a4537a85337b0
Leonard Follner <leonard.follner@gmail.com>
2018-01-18 10:25:11 +0100

    TASK: updated data-structure

--- src/Components/DetailMain.js
+++ src/Components/DetailMain.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 
 class DetailMain extends Component {
   render() {
-    return (
-      <div className="main-container">
-        <p>I am detail main</p>
-      </div>
-    );
+
+    const term = this.props.termInDetailsView;
+    const changes = term.changes;
+
+    return(
+      changes.map((currentValue, index) => {
+      return (
+        <DetailSection name={currentValue} id={currentValue.index} sectionsCount={changes.length} key={index}/>
+      );
+      }
+
+    ));
+
 
   }
 }
