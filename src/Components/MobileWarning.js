074d62a7f47b09a7e7bfac8e87bb15754bbaa329
Leonard Follner <leonard.follner@gmail.com>
2018-01-31 20:22:23 +0100

    TASK: added exitButton to MobileWarning, disabled by default

--- src/Components/MobileWarning.js
+++ src/Components/MobileWarning.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import classnames from "classnames";
 
+import {mobileWarningMayBeIgnored} from "../settings";
+import ExitButton from "./ExitButton";
+
 class MobileWarning extends Component {
   render() {
     const className = classnames("mobile-warning");
-    return <div className={className}>Diese Anwendung ist nicht für eine mobile Nutzung optimiert. Bitte benutz ein
-      Gerät mit einem größeren Display.</div>
+    return (
+      <div>
+        {mobileWarningMayBeIgnored ? <ExitButton onClick={this.props.onExitButtonClick}/> : ''}
+        <div className={className}>
+          Diese Anwendung ist nicht für eine mobile Nutzung optimiert. Bitte benutz ein Gerät mit einem größeren
+          Display.
+        </div>
+      </div>
+    )
   }
 }
 
--- src/Components/Prototyp.js
+++ src/Components/Prototyp.js
@@ -1,18 +1,38 @@
 import React, {PureComponent} from 'react';
 import {connect} from "react-redux";
 
-import {selectors} from "../Redux";
+import {actions, selectors} from "../Redux";
 import SideBar from "./SideBar";
 import MainContainer from "./MainContainer";
-import ExitButton from "./DetailView/ExitButton";
+import ExitButton from "./ExitButton";
 import MobileWarning from "./MobileWarning";
+import {minimumDeviceWidthNeeded, mobileWarningMayBeIgnored} from "../settings";
 
 class Prototyp extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ignoreMobileWarning: false
+    }
+  }
+
+  handleExitWarning() {
+    return () => {
+      if (mobileWarningMayBeIgnored) {
+        this.setState({
+          ignoreMobileWarning: true
+        });
+      }
+    };
+  }
+
   render() {
     return (
       <div className="prototyp">
-        {window.innerWidth < 750 ? <MobileWarning/> : ''}
-        {this.props.isDetailsViewVisible ? <ExitButton/> : ''}
+        {window.innerWidth < minimumDeviceWidthNeeded && !this.state.ignoreMobileWarning ?
+          <MobileWarning onExitButtonClick={this.handleExitWarning()}/> : ''}
+        {this.props.isDetailsViewVisible ? <ExitButton onClick={this.props.exitDetailsView}/> : ''}
         <MainContainer detailsView={this.props.isDetailsViewVisible}/>
         <SideBar detailsView={this.props.isDetailsViewVisible}/>
       </div>
@@ -26,4 +46,10 @@ const mapStateToProps = state => {
   }
 };
 
-export default connect(mapStateToProps)(Prototyp);
+const mapDispatchToProps = dispatch => {
+  return {
+    exitDetailsView: () => dispatch(actions.UI.DetailsView.exitDetailsView()),
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Prototyp);
