54ac315f6ed3ae87c8db4d36b69c559193ff9992
Leonard Follner <leonard.follner@gmail.com>
2018-03-03 19:28:23 +0100

    TASK: the drag-image is now shaped like a marker

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -4,9 +4,67 @@ import {actions} from "../../Redux/index";
 import {connect} from "react-redux";
 
 class Card extends Component {
+  // copied and modified from mobile-drag-drop
+  prepareNodeCopyAsDragImage = (srcNode, dstNode) => {
+
+    // Is this node an element?
+    if (srcNode.nodeType === 1) {
+
+      // Clone the style
+      const cs = getComputedStyle(srcNode);
+      for (let i = 0; i < cs.length; i++) {
+        const csName = cs[i];
+        switch (csName) {
+          case "width":
+          case "height":
+            dstNode.style.setProperty(csName, "135px", cs.getPropertyPriority(csName));
+            break;
+          case "left":
+          case "right":
+          case "top":
+          case "bottom":
+            dstNode.style.setProperty(csName, "235px", cs.getPropertyPriority(csName));
+            break;
+          default:
+            dstNode.style.setProperty(csName, cs.getPropertyValue(csName), cs.getPropertyPriority(csName));
+        }
+      }
+      dstNode.style.setProperty("border-radius", "50%");
+
+      // no interaction with the drag image, pls! this is also important to make the drag image transparent for hit-testing
+      // hit testing is done in the drag and drop iteration to find the element the user currently is hovering over while dragging.
+      // if pointer-events is not none or a browser does behave in an unexpected way than the hit test transparency on the drag image
+      // will break
+      dstNode.style.pointerEvents = "none";
+
+      // Remove any potential conflict attributes
+      dstNode.removeAttribute("id");
+      dstNode.removeAttribute("class");
+      dstNode.removeAttribute("draggable");
+    }
+
+    // Remove all children
+    if (srcNode.hasChildNodes()) {
+      dstNode.innerHTML = '';
+    }
+  };
+
+  createDragImage = (sourceNode) => {
+
+    const dragImage = sourceNode.cloneNode(true);
+
+    // this removes any id's and stuff that could interfere with drag and drop
+    this.prepareNodeCopyAsDragImage(sourceNode, dragImage);
+
+    document.body.appendChild(dragImage);
+
+    return dragImage;
+  };
+
   handleDragStart = event => {
     event.dataTransfer.setData("text/plain", this.props.term.id.toString());
     this.setState({isBeingDragged: true});
+    event.dataTransfer.setDragImage(this.createDragImage(this.elem), 135 / 2, 135 / 2);
     this.props.toggleCardIsBeingDragged(this.props.term.targetRegion);
   };
   handleDragEnd = () => {
@@ -42,7 +100,8 @@ class Card extends Component {
     };
 
     return (
-      <div className={className} draggable={true} onDragStart={this.handleDragStart} onDragEnd={this.handleDragEnd}
+      <div className={className} ref={elem => this.elem = elem} draggable={true} onDragStart={this.handleDragStart}
+           onDragEnd={this.handleDragEnd}
            style={style}>
         <h2>{this.props.term.name}</h2>
       </div>
